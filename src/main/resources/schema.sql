7cec48057e0e9b0f3122a21753229e1d891e0492
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-06-07 19:31:15 +0300

    Postgresql added

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,10 @@
+drop table ingredients;
+drop table products;
+drop table clients;
+drop table orders;
+drop table order_to_product;
+drop table custom_to_order;
+
 create table if not exists ingredients (
 id varchar(4) not null,
 name varchar(20) not null,
@@ -6,7 +13,7 @@ price varchar(10) not null
 );
 
 create table if not exists products (
-id int not null auto_increment,
+id serial,
 name varchar(32) not null,
 price varchar(8) not null,
 ingredients varchar(256) not null,
@@ -15,7 +22,7 @@ image varchar(256) not null
 );
 
 create table if not exists clients (
-id int not null auto_increment,
+id serial,
 name varchar(32) not null,
 lastname varchar(32) not null,
 mail varchar(32) not null,
@@ -25,21 +32,21 @@ cardnumber varchar(16) not null
 );
 
 create table if not exists orders (
-id int not null auto_increment,
-clientId int not null,
-customId int not null
+id serial,
+clientId varchar(16) not null,
+customId varchar(16) not null
 );
 
 create table if not exists order_to_product (
-id int not null auto_increment,
-orderId int not null,
-productId int not null,
+id serial,
+orderId varchar(16) not null,
+productId varchar(16) not null,
 amount varchar(16) not null
 );
 
 create table if not exists custom_to_order (
-id int not null auto_increment,
-orderId int not null,
-ingredientId int not null,
-pieId int not null
+id serial,
+orderId varchar(16) not null,
+ingredientId varchar(16) not null,
+pieId varchar(16) not null
 );
